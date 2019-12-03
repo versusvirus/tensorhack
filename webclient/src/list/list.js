2af83b2616ca9023a9dde3a3ac068f3c20553c92
begunov.av <begunov.av@gmail.com>
2019-12-03 19:32:51 +0300

    design

--- webclient/src/list/list.js
+++ webclient/src/list/list.js
@@ -21,7 +21,11 @@ import PersonIcon from '@material-ui/icons/Person';
 import AddIcon from '@material-ui/icons/Add';
 import Typography from '@material-ui/core/Typography';
 import { blue } from '@material-ui/core/colors';
-
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import Checkbox from '@material-ui/core/Checkbox';
+import Fab from '@material-ui/core/Fab';
+import DehazeIcon from '@material-ui/icons/Dehaze';
 
 
 const STextField = styled(TextField)({
@@ -33,11 +37,11 @@ const STextField = styled(TextField)({
 
 const SList = styled(List)({
     width: '100%',
-    maxWidth: 360,
+    maxWidth: 760,
     backgroundColor: 'white',
     position: 'relative',
     overflow: 'auto',
-    maxHeight: 300,
+    maxHeight: 700,
   })
 
 
@@ -79,8 +83,31 @@ class TodoList extends React.Component {
             }],
             newValue: '',
             open:false,
-            purchases:[{"price":16,"shop":"Пятерочка","products":[{"_id":"5dcee5601c9d44000067f295","shop":"Пятерочка","price":13,"name":"Молоко"},{"_id":"5dcee59b1c9d44000067f299","shop":"Пятерочка","price":3,"name":"Яйцо"}]}]
-        };
+            purchases: [{
+                "_id": "33e333",
+                "shop": "Пятерочка",
+                "products": [{
+                    "_id": "5dcee5601c9d44000067f295",
+                    "shop": "Пятерочка",
+                    "price": "53,99",
+                    "stockprice": "89,50",
+                    "name": 'Молоко "Домик в деревне"',
+                    "img" : "https://leonardo.edadeal.io/dyn/re/items/248/orig/6072e0b3.jpg",
+                    "characteristics": ["0,9л", "3.2%", "59,98 ₽/л"],
+                    "count":2,
+                    "unit":"бут"
+                }, {
+                    "_id": "5dcee59b1c9d44000067f299",
+                    "shop": "Пятерочка",
+                    "price": "69,90",
+                    "stockprice": "84,90",
+                    "name": 'Яйцо столовое "Дедушкины яйца"',
+                    "img" : "https://leonardo.edadeal.io/dyn/re/items/19/orig/ec9b9cff.jpg",
+                    "characteristics": ["10шт", "отборное", "6,99 ₽/шт"],
+                    "count":1,
+                    "unit":"уп"
+                }]
+            }]        };
         this.onAddHandler = this.onAddHandler.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleClose = this.handleClose.bind(this);
@@ -147,31 +174,79 @@ class TodoList extends React.Component {
     }
 
     render() {
-
+        const ulstyle = {margin:'0px', padding:'0px'};
+        const avatarstyle = {width:'60px', height:'60px'};
+    
         return (
             <div>
 
                 <div class="d-flex flex-wrap">
 
-                <Button onClick={this.onAddHandler} variant="contained" className="button">Добавить продукт</Button>
+                <Button onClick={this.onAddHandler} variant="contained" className="button" style={{width:"100%"}}>Добавить продукт</Button>
                 <SimpleDialog selectedValue={null} open={this.state.open} onClose={this.handleClose} />
                 
                 </div>
                 <SList subheader={<li />}>
                      {this.state.purchases.map(purchase => (
         <li key={`section-${purchase.shop}`} className={''}>
-          <ul className={'classes.ul'}>
-            <ListSubheader>{`${purchase.shop}`}</ListSubheader>
+          <ul className={'classes.ul'} style={ulstyle}>
+            <ListSubheader style={{"text-align": "left", "lin11e-height": "32px", "background-color":"rgb(245,245, 245)"}}>
+                {`${purchase.shop}`}
+                <span style={{color:"rgba(127, 127, 127, 0.54)"}}> 145  ₽</span>
+            </ListSubheader>
             
             {purchase.products.map(product => (
               <ListItem key={`item-${purchase.shop}-${product._id}`}>
-                <ListItemText primary={`${product.name}`} />
+                <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src={product.img} variant="rounded" style={avatarstyle}/>
+                </ListItemAvatar>
+                <ListItemText
+                    primary={`${product.name}`} 
+                    secondary={
+                        <React.Fragment>
+                        {product.characteristics.map(characteristic => (
+                            <span className="item-characteristic">{characteristic}</span>
+                        ))}
+                        <div>
+                            <span className="item-price">{product.price} &#8381;</span>
+                            <span className="item-stockprice">{product.stockprice} &#8381;</span>
+                            <div style={{float:"right"}}>{product.count} {product.unit}</div>
+                        </div>
+                        </React.Fragment>
+                    }
+                />
               </ListItem>
             ))}
+            <ListItem>
+                <ListItemText primary={
+                    <React.Fragment>
+                        Итого: <span style={{"font-size":"24px"}}>1234.66 ₽</span>
+                    </React.Fragment>
+                    }></ListItemText>
+            </ListItem>
           </ul>
         </li>
       ))}
     </SList>
+    <div style={{"text-align":"left", float:"left"}}>
+    <FormControlLabel 
+        control={
+        <Checkbox
+        /*checked={state.checkedA}
+        onChange={handleChange('checkedA')}
+       */ value="checkedA"
+        inputProps={{
+          'aria-label': 'primary checkbox',
+        }}></Checkbox>
+        }
+        label="В одном магазине"
+      />
+
+    </div>
+    <Fab color="primary" aria-label="add" size="small" style={{float:"right"}}>
+        <DehazeIcon />
+      </Fab>
+
    </div>
         );
     }
